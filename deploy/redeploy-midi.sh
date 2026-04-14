d83593695a3d963005ad7e87fa7dbcd3b408377c
Yewen Jin <yewen.elsa.jin@gmail.com>
2026-04-14 21:39:00 +0100

    deploy: add redeploy script for VPS

---
+++ deploy/redeploy-midi.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+cd ~/remote-midi
+git pull
+
+cd /srv/reverse-proxy
+docker compose up -d --build midi-relay
+
+echo "Done. Checking health..."
+sleep 2
+docker logs midi-relay --tail 5
